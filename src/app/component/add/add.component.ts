d772e680618a14ad8ecd9b9daf49bd03c5166066
DAXILSAXENA <daxildaxil@gmail.com>
2021-09-03 07:19:22 +0530

    Added pipe for Date and salary conversion,frontend validators,snackbar

--- src/app/component/add/add.component.ts
+++ src/app/component/add/add.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, NgSelectOption, Validators } from '@angular/forms';
 import { MatCheckboxChange } from '@angular/material/checkbox';
+import { MAT_DATEPICKER_VALIDATORS } from '@angular/material/datepicker';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Employee } from 'src/app/model/employee';
 import { DataService } from 'src/app/service/data.service';
@@ -15,6 +17,8 @@ export class AddComponent implements OnInit {
 
   public employee: Employee = new Employee;
   public employeeFormGroup: FormGroup;
+  minDate: Date;
+  maxDate: Date;
 
   departments: Array<any> = [
     {
@@ -44,17 +48,22 @@ export class AddComponent implements OnInit {
     private httpService: HttpService,
     private activatedRoute: ActivatedRoute,
     private dataService: DataService,
-    private router: Router) {
+    private router: Router,
+    private snackBar: MatSnackBar) {
     this.employeeFormGroup = this.fb.group({
-      name: new FormControl(''),
-      profilePic: new FormControl(''),
-      gender: new FormControl(''),
+      name: new FormControl('',[Validators.pattern("^[A-Z]{1}[a-zA-Z\\s]{2,}$"),Validators.required]),
+      profilePic: new FormControl('',Validators.required),
+      gender: new FormControl('',Validators.required),
       department: this.fb.array([], [Validators.required]),
-      salary: new FormControl(''),
-      createdDate: new FormControl(''),
-      note: new FormControl('')
+      salary: new FormControl('',Validators.required),
+      createdDate: new FormControl('',Validators.required),
+      note: new FormControl('',Validators.required)
     })
-  }
+      // Set maximum Date of joining to today's Date
+      const currentYear = new Date().getFullYear();
+      this.minDate = new Date(currentYear - 0, 0, 1);
+      this.maxDate = new Date();
+    }
 
   ngOnInit(): void {
     if (this.activatedRoute.snapshot.params['id'] != undefined) {
@@ -115,6 +124,7 @@ export class AddComponent implements OnInit {
       this.httpService.updateEmployeeData(this.employee, this.activatedRoute.snapshot.params['id']).subscribe(response => {
         console.log(response);
         this.router.navigateByUrl('/home');
+        this.snackBar.open("Form Updated Successfully","Updated",{duration:3000});
       });
 
     }
@@ -122,8 +132,10 @@ export class AddComponent implements OnInit {
       this.httpService.addEmployeeData(this.employee).subscribe(response => {
         console.log(response);
         this.router.navigateByUrl('/home');
+        this.snackBar.open("Form Submitted Successfully","Submitted",{duration:3000});
       });
     }
+    
 
   }
 }
